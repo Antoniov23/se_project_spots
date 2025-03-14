41ebfa9857c9f915b7891b088aea1617b9b13f1a
antoniovazquez <tonyvazquez722@gmail.com>
2025-03-13 20:24:52 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,19 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const editModal = document.querySelector("#edit-profile-modal");
+const editModalClosebutton = editModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+  editModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+editModalClosebutton.addEventListener("click", closeModal);
