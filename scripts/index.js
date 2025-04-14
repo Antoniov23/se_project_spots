1029c7d2f13cc1fd9c86fd7eb949cd5704c4eebe
antoniovazquez <tonyvazquez722@gmail.com>
2025-04-14 15:31:36 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 const initialCards = [
   {
+    name: "Golden Gate Bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
+  {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
@@ -25,9 +29,16 @@ const initialCards = [
   },
 ];
 
+const cardsList = document.querySelector(".cards__list");
 const addCardModal = document.querySelector("#add-card-modal");
 const addCardForm = document.forms["cardForm"];
 const addCardButton = document.querySelector(".profile__add-button");
+
+const previewModal = document.querySelector("#preview-modal");
+const previewModalCloseButton = previewModal.querySelector(".modal__close-btn");
+previewModalCloseButton.addEventListener("click", () =>
+  closeModal(previewModal)
+);
 
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileName = document.querySelector(".profile__name");
@@ -41,13 +52,13 @@ const editModalDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 
-const cardTemplate = document.querySelector("#card-template");
-const cardsList = document.querySelector(".cards__list");
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
 
 function getCardElement(data) {
-  const cardElement = cardTemplate.content
-    .querySelector(".card")
-    .cloneNode(true);
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitleEl = cardElement.querySelector(".card__title");
 
   const cardNameEl = cardElement.querySelector(".card__title");
   const cardImageEl = cardElement.querySelector(".card__image");
@@ -57,8 +68,26 @@ function getCardElement(data) {
   cardImageEl.src = data.link;
   cardImageEl.alt = data.name;
 
-  cardButtonEl.addEventListener("click", function () {
-    cardButtonEl.classList.toggle("card__like-button_is-active");
+  cardImageEl.addEventListener("click", () => {
+    const previewModal = document.querySelector("#preview-modal");
+    const previewImage = previewModal.querySelector(".modal__image");
+    const previewTitle = previewModal.querySelector(".modal__heading");
+
+    previewImage.src = data.link;
+    previewImage.alt = data.name;
+    previewTitle.textContent = data.name;
+
+    openModal(previewModal);
+  });
+
+  const CardLikeBtnEl = cardElement.querySelector(".card__like-button");
+  CardLikeBtnEl.addEventListener("click", () => {
+    CardLikeBtnEl.classList.toggle("card__like-button_active");
+  });
+
+  const CardDeleteBtnEl = cardElement.querySelector(".card__delete-button");
+  CardDeleteBtnEl.addEventListener("click", () => {
+    CardDeleteBtnEl.closest(".card").remove();
   });
 
   return cardElement;
@@ -71,8 +100,10 @@ function renderCard(cardData) {
 
 function openModal(modal) {
   modal.classList.add("modal_opened");
-  editModalNameInput.value = profileName.textContent;
-  editModalDescriptionInput.value = profileDescription.textContent;
+  if (modal === editModal) {
+    editModalNameInput.value = profileName.textContent;
+    editModalDescriptionInput.value = profileDescription.textContent;
+  }
 }
 
 function closeModal(modal) {
@@ -98,7 +129,7 @@ addCardForm.addEventListener("submit", (e) => {
 
   const title = addCardForm.querySelector("#card-title-input").value;
   const imageUrl = addCardForm.querySelector("#card-url-input").value;
-
+  console.log("Form submitted", { title, imageUrl });
   const newCard = {
     name: title,
     link: imageUrl,
@@ -109,6 +140,11 @@ addCardForm.addEventListener("submit", (e) => {
   closeModal(addCardModal);
   addCardForm.reset();
 });
+
+const addCardModalCloseButton = addCardModal.querySelector(".modal__close-btn");
+addCardModalCloseButton.addEventListener("click", () =>
+  closeModal(addCardModal)
+);
 
 initialCards.forEach((cardData) => {
   const cardElement = getCardElement(cardData);
